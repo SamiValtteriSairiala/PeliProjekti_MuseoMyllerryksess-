c2331ef001cd2e83dd753403affb06b6898c4630
SamiValtteriSairiala <97952330+SamiValtteriSairiala@users.noreply.github.com>
2022-02-09 10:54:08 +0200

    Initial commit

---
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+    public class MainMenuController : MonoBehaviour
+    {
+        
+        [SerializeField] private GameObject startButton;
+        [SerializeField] private GameObject SettingsMenu;
+        [SerializeField] private GameObject MainMenu;
+        // Start is called before the first frame update
+        void Start()
+        {
+            SettingsMenu.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // If settingmenu is active.
+            if(SettingsMenu.activeInHierarchy == true)
+            {
+                // If user presses back button on android Settings menu closes.
+                if(Input.GetKeyDown(KeyCode.Escape))
+                {
+                    SettingsMenu.SetActive(false);
+                    MainMenu.SetActive(true);
+                }
+            }
+            if(MainMenu.activeInHierarchy == true)
+            {
+                 if(Input.GetKeyDown(KeyCode.Escape))
+                {
+                    Application.Quit();
+                }
+            }
+        }
+
+        void PlayButton()
+        {
+            // Change SampleScene to our main play scene!
+            SceneManager.LoadScene("SampleScene");
+        }
+
+        void SettingsButton()
+        {
+            //Activate settingsmenu canvas.
+            SettingsMenu.SetActive(true);
+            MainMenu.SetActive(false);
+        }
+
+        void CloseSettingsButton()
+        {
+            SettingsMenu.SetActive(false);
+            MainMenu.SetActive(true);
+            
+        }
+
+        void Mute()
+        {
+            // If audio is higher than 0 then mute listener.
+            if(AudioListener.volume > 0)
+            {
+                AudioListener.volume = 0;
+            }
+            if(AudioListener.volume < 0)
+            {
+                AudioListener.volume = 1;
+            }
+            
+        }
+    }
+
