5e19c73bcf0370a933782771aa3ccf0bcc980766
SamiValtteriSairiala <97952330+SamiValtteriSairiala@users.noreply.github.com>
2022-02-22 11:35:51 +0200

    Added Basic movement
    Added movement between Lobby, outside, cloakroom and upperfloor.
    
    TRIED REMOVING SHADERCOMPILLER

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
 
-    [SerializeField] private GameObject MoveArrowUp;
+    [SerializeField] private GameObject LockedText;
+    // Add all "playable rooms" here
+    [SerializeField] private GameObject OutsideScreen;
     [SerializeField] private GameObject LobbyScreen;
+    [SerializeField] private GameObject UpperFloor;
+    [SerializeField] private GameObject Cloakroom;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +24,50 @@ public class PlayerMove : MonoBehaviour
     }
 
     void MoveArrowUpwards(){
-        if(LobbyScreen.activeInHierarchy == true){
-            LobbyScreen.active = false;
-            
-
+        //When pressed arrow pointing upwards then check what "scene" is active and change it to next one.
+        if(OutsideScreen.activeInHierarchy == true){
+            OutsideScreen.SetActive(false);
+            LobbyScreen.SetActive(true);
         }
+
+
+        if(LobbyScreen.activeInHierarchy == true){
+            LobbyScreen.SetActive(false);
+            UpperFloor.SetActive(true);  
+        }
+    }
+
+    void MoveArrowRight(){
+         //When pressed arrow pointing right then check what "scene" is active and change it to next one.
+        if(LobbyScreen.activeInHierarchy == true){
+            LobbyScreen.SetActive(false);
+            Cloakroom.SetActive(true);
+        }
+
+    }
+    
+    void MoveArrowLeft(){
+        //When pressed arrow pointing left then check what "scene" is active and change it to next one.
+        if(Cloakroom.activeInHierarchy == true){
+            Cloakroom.SetActive(false);
+            LobbyScreen.SetActive(true);
+        }
+    }
+
+    void MoveArrowDown(){
+        if(UpperFloor.activeInHierarchy == true){
+            UpperFloor.SetActive(false);
+            LobbyScreen.SetActive(true);
+        }
+
+        if(LobbyScreen.activeInHierarchy == true){
+            LockedText.SetActive(true);
+            //Show text for 5 seconds, then disab
+            Invoke("DisableText", 5f);
+        }
+    }
+
+    void DisableText(){
+        LockedText.SetActive(false);
     }
 }
