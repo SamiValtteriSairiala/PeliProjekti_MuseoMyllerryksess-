d57a894f697809c9207bbee32bcaf63d7999487b
SamiValtteriSairiala <97952330+SamiValtteriSairiala@users.noreply.github.com>
2022-03-07 11:46:33 +0200

    Added ability to open safetybox

--- Assets/Scripts/RoomScripts/CloakRoom/CloakRoomMove.cs
+++ Assets/Scripts/RoomScripts/CloakRoom/CloakRoomMove.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CloakRoomMove : MonoBehaviour
 {
     [SerializeField] private GameObject CloakRoom;
     [SerializeField] private GameObject LobbyScreen;
+    [SerializeField] private GameObject SafetyBox;
+    [SerializeField] private GameObject CloakRoomImage;
 
     // Start is called before the first frame update
     void Start()
@@ -22,5 +25,16 @@ public class CloakRoomMove : MonoBehaviour
     public void MoveLeft(){
         CloakRoom.SetActive(false);
         LobbyScreen.SetActive(true);
+    }
+
+
+    public void OpenSafetyBox(){
+        SafetyBox.SetActive(true);
+        CloakRoomImage.SetActive(false);
+    }
+
+    public void BackToCloakRoom(){
+        CloakRoomImage.SetActive(true);
+        SafetyBox.SetActive(false);
     }
 }
