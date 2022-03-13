69fc37aa84fedaba091d9da972c3eaedbb039514
SamiValtteriSairiala <97952330+SamiValtteriSairiala@users.noreply.github.com>
2022-03-13 15:26:29 +0200

    Puzzle somewhat done

---
+++ Assets/Scripts/PuzzleScripts/TileScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public Vector3 TargetPos;
+    private Vector3 correctPos;
+    public int number;
+    public bool inRightPlace;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        TargetPos = transform.position;
+        correctPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, TargetPos, 0.05f);
+        if (TargetPos == correctPos)
+        {
+            inRightPlace = true;
+        }
+        else
+        {
+            inRightPlace = false;
+        }
+    }
+}
