5b5a2fdd48b45f55f246ab6390153c2713df0457
SamiValtteriSairiala <97952330+SamiValtteriSairiala@users.noreply.github.com>
2022-04-05 10:46:13 +0300

    Added pausemenu to main scene

--- Assets/Scripts/MenuScripts/SoundManager.cs
+++ Assets/Scripts/MenuScripts/SoundManager.cs
@@ -10,58 +10,68 @@ public class SoundManager : MonoBehaviour
     public bool SFXisMuted = false;
     public AudioMixer MusicMixer;
     public AudioMixer SFXMixer;
-    
+
     [SerializeField] private Toggle ToggleMusic;
     [SerializeField] private Toggle ToggleSFX;
     // Start is called before the first frame update
     void Start()
     {
-        
+
         DontDestroyOnLoad(this.gameObject);
     }
 
-    
+
 
     // Update is called once per frame
     void Update()
     {
         //For button usage.
-        if(MusicIsMuted == true){
+        if (MusicIsMuted == true)
+        {
             MuteMusic();
         }
-        if(MusicIsMuted == false && ToggleMusic.isOn == false){
+        if (MusicIsMuted == false && ToggleMusic.isOn == false)
+        {
             UnMuteMusic();
         }
-        if(SFXisMuted == true){
+        if (SFXisMuted == true)
+        {
             MuteSFX();
         }
-        if(SFXisMuted == false && ToggleSFX.isOn == false){
+        if (SFXisMuted == false && ToggleSFX.isOn == false)
+        {
             UnMuteSFX();
         }
     }
 
-    public void MuteMusic(){
+    public void MuteMusic()
+    {
         MusicMixer.SetFloat("MusicVolume", -80f);
         MusicIsMuted = true;
 
-        if(MusicIsMuted == true && ToggleMusic.isOn == true){
+        if (MusicIsMuted == true && ToggleMusic.isOn == true)
+        {
             MusicMixer.SetFloat("MusicVolume", 0f);
             MusicIsMuted = false;
         }
     }
-    public void UnMuteMusic(){
+    public void UnMuteMusic()
+    {
         MusicMixer.SetFloat("MusicVolume", 0f);
         MusicIsMuted = false;
     }
-    public void MuteSFX(){
+    public void MuteSFX()
+    {
         SFXMixer.SetFloat("SFXVolume", -80f);
         SFXisMuted = true;
-        if(SFXisMuted == true && ToggleSFX.isOn == true){
+        if (SFXisMuted == true && ToggleSFX.isOn == true)
+        {
             SFXMixer.SetFloat("SFXVolume", 0f);
             SFXisMuted = false;
         }
     }
-    public void UnMuteSFX(){
+    public void UnMuteSFX()
+    {
         SFXMixer.SetFloat("SFXVolume", 0f);
         SFXisMuted = false;
     }
