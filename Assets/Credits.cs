ef0a58dbbc79f7c5f85bb552cdd24f195c41077e
SamiValtteriSairiala <97952330+SamiValtteriSairiala@users.noreply.github.com>
2022-04-21 10:19:11 +0300

    added credits button to museum finish screens

--- Assets/Credits.cs
+++ Assets/Credits.cs
@@ -5,20 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
+    public GameObject SoundManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SoundManager = GameObject.Find("SoundManager");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void MainMenu(){
+    public void MainMenu()
+    {
+        Destroy(SoundManager);
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void CreditsOpen()
+    {
+        Destroy(SoundManager);
+        SceneManager.LoadScene("Credits");
     }
 
 }
